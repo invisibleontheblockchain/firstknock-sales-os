a40b5f99011ffa29a677a4782ce6d98831488d96
Cory Larson <baysecurity@gmail.com>
2026-01-08 04:39:48 +0000

    File changes

--- src/components/routes/RouteMapPreview.jsx
+++ src/components/routes/RouteMapPreview.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Polyline, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { MapPin } from 'lucide-react';
 
 // Custom Route Marker Icons
 const createNumberedIcon = (number, color) => new L.DivIcon({
