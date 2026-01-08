03ac750a2fda2f18c85dcda6594c913645d2012c
Cory Larson <baysecurity@gmail.com>
2026-01-08 04:46:35 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,19 +1,9 @@
-import Home from './pages/Home';
-import List from './pages/List';
-import Sync from './pages/Sync';
-import Routes from './pages/Routes';
-import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Home": Home,
-    "List": List,
-    "Sync": Sync,
-    "Routes": Routes,
 }
 
 export const pagesConfig = {
     mainPage: "Home",
     Pages: PAGES,
-    Layout: __Layout,
 };
